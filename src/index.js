fe4de07f533c86c97cf36ac0d6ee53e03eb21243
Felipe Nuñez <pipeenun@gmail.com>
2022-06-02 22:42:39 -0400

    Se modifica archivo Index.js. se elimina contenido y se crea un programa para mostrar la fecha y hora. actualizandose cada segundo.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
-// const element = document.createElement("h1"); // creamos elemento
-// element.innerText = "JovenesProgramadores"; // el texto que pondremos
-
-// const container = document.getElementById('root'); // creamos una variable que obtiene la ID
-
-// container.appendChild(element); // que queremos mostrar
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-// estas dos herramientas trabajarán en conjunto: React como análogo a createElement y ReactDOM a appendChild
 
-// creamos una variable
-const name = 'Felipe';
+const style = {
+    color: "red"
+}
 
-const element = <h1>¡Bienvenido, {name}, a React en Jovenes Programadores! </h1>
+function tictac () {
+    const element = (
+        <div>
+            <h1 style={style}> Reloj en React! </h1>
+            <h1> son las {new Date().toLocaleTimeString()}. </h1>
+            <h1> la fecha es la stge. {new Date().toLocaleDateString()}. </h1>
+            {/* toLocaleTimeString, mostrará la hora. toLocaleDateString mostrará la fecha */}
+        </div>
+    );
+    const container = document.getElementById('root');
+    ReactDOM.render(element, container);
+}
 
-//const element = <h1> ¡React en Jovenes Programadores! </h1>;
-
-const container = document.getElementById("root");
-
-ReactDOM.render(element, container);
-// ahora, lo que haremos diferente a lo anterior será el "appendChild."
-// utilizaremos el método ReactDOM.render el cual está compuesto de 2 argumentos.
-// ¿qué queremos renderizar? y ¿Dónde lo queremos renderizar?
+setInterval(tictac,1000);
