a5b4b4e6088ceefde1e5df04e86bc501f23f12e9
Felipe Nuñez <pipeenun@gmail.com>
2022-06-02 22:28:48 -0400

    de carpeta PUBLIC se modifica archivo index.html y de carpeta SRC se modifica archivo index.js . se agregan comentarios y se modifica codigo de archivo index.js para pasarlo a React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
+// const element = document.createElement("h1"); // creamos elemento
+// element.innerText = "JovenesProgramadores"; // el texto que pondremos
+
+// const container = document.getElementById('root'); // creamos una variable que obtiene la ID
+
+// container.appendChild(element); // que queremos mostrar
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+// estas dos herramientas trabajarán en conjunto: React como análogo a createElement y ReactDOM a appendChild
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// creamos una variable
+const name = 'Felipe';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const element = <h1>¡Bienvenido, {name}, a React en Jovenes Programadores! </h1>
+
+//const element = <h1> ¡React en Jovenes Programadores! </h1>;
+
+const container = document.getElementById("root");
+
+ReactDOM.render(element, container);
+// ahora, lo que haremos diferente a lo anterior será el "appendChild."
+// utilizaremos el método ReactDOM.render el cual está compuesto de 2 argumentos.
+// ¿qué queremos renderizar? y ¿Dónde lo queremos renderizar?
